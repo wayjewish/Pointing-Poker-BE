9b596b6f12a3ed9196486d50eda89b0839380693
Dmitry Uvarov <diman_niks@mail.ru>
2021-09-05 23:44:09 +0300

    feat: socket.io

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,42 @@
 import * as dotenv from 'dotenv';
-
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
 import express from 'express';
 import cors from 'cors';
 
 dotenv.config();
-const port = process.env.PORT || 3000;
-
+const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
-
 app.use(express.json());
 
-app.listen(port, () => console.log('server started'));
+const httpServer = createServer();
+const io = new Server(httpServer);
+
+io.on('connection', (socket: Socket) => {
+  console.log(socket.id);
+
+  socket.on('createGame', (user) => {
+    // сгенерировать id игры
+    // создать игру с диллером = user
+    // вернуть объект игры
+  });
+
+  socket.on('loginGame', (user) => {
+    // войти в игру
+    // добавить игрока user
+    // вернуть объект игры
+  });
+
+  socket.on('closeGame', () => {
+    // закрыть игру
+  });
+});
+
+app.get('/', (req, res) => {
+  res.send('Server is up and running');
+});
+
+httpServer.listen(PORT, () => {
+  console.log(`Listening to ${PORT}`);
+});
